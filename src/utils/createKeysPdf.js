22fcb000c35d0ca79508787179981d0a43328bb0
mrhoba9 <amadoehab@gmail.com>
2025-10-11 21:31:54 +0300

    url added in pdf

--- src/utils/createKeysPdf.js
+++ src/utils/createKeysPdf.js
@@ -318,7 +318,7 @@ export async function createKeysPdf(publicKey, privateKey, signature) {
 
   y -= 15;
 
-  page.drawText(`${URL}/verify`, {
+  page.drawText(`${URL}/#/verify`, {
     x: margin,
     y,
     size: 9,
